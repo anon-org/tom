b9e318667b5c77b05eb599aa53534dd872324526
ClavinJune <cjuneardo@gmail.com>
2020-01-23 00:57:29 +0700

    refactor Has func

--- helper/helper.go
+++ helper/helper.go
@@ -8,18 +8,16 @@ func Has(haystack interface{}, needle interface{}) bool {
 	arr := reflect.ValueOf(haystack)
 	length := arr.Len()
 
-	if length == 0 {
+	if length == 0 || arr.Kind() != reflect.Slice {
 		return false
 	}
 
-	if arr.Kind() == reflect.Slice {
-		for i, j := 0, length-1; i <= j; i, j = i+1, j-1 {
-			hay1 := arr.Index(i).Interface()
-			hay2 := arr.Index(j).Interface()
+	for i, j := 0, length-1; i <= j; i, j = i+1, j-1 {
+		hay1 := arr.Index(i).Interface()
+		hay2 := arr.Index(j).Interface()
 
-			if hay1 == needle || hay2 == needle {
-				return true
-			}
+		if hay1 == needle || hay2 == needle {
+			return true
 		}
 	}
 
